e64482e20194e5b77842693ea747e9b375a55660
luozhilin <luozhiping@luozhilin.local>
2015-09-18 09:46:30 +0800

    你妹修改

--- HelloWorld/HelloWorld/ViewController.m
+++ HelloWorld/HelloWorld/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 //第一次修改
+//你妹修改
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
